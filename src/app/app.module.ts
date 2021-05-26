31cf666260327487db97105646daf6be6cef46a5
Wayne <wayne@gamesmartltd.com>
2021-05-26 14:16:22 +0800

    add head bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainDivComponent } from '../siene/main-div/main-div.component';
-import { LobbyComponent } from 'src/siene/lobby/lobby.component';
+import { LobbyComponent } from '../siene/lobby/lobby.component';
 import { LoadingAndPoComponent } from '../siene/loading-and-po/loading-and-po.component';
 import { DynamicLayerComponent } from '../siene/dynamic-layer/dynamic-layer.component';
 import { PageDirective } from '../siene/dynamic-layer/page.directive';
+import { HeadBarComponent } from '../siene/video/head-bar/head-bar.component'
+import { VideoComponent } from '../siene/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainDivComponent,
-    LobbyComponent,
     LoadingAndPoComponent,
     DynamicLayerComponent,
-    PageDirective
+    PageDirective,
+    LobbyComponent,
+    VideoComponent,
+    HeadBarComponent
    ],
   imports: [
     BrowserModule
   ],
+  entryComponents: [ LobbyComponent, VideoComponent, HeadBarComponent ],
   providers: [],
   bootstrap: [AppComponent]
 })
