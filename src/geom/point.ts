7e5341bcc6cd2ce4a99468edef4c0ca92618bdc4
Wayne <wayne@gamesmartltd.com>
2021-05-27 15:20:24 +0800

    add Point

---
+++ src/geom/point.ts
@@ -0,0 +1,56 @@
+export class Point {
+  x: number;
+  y: number;
+  constructor( x: number = 0, y: number = 0 ){
+    this.x = x;
+    this.y = y;
+  }
+
+  get length(): number{
+    return Math.sqrt( Math.pow( this.x, 2 ) + Math.pow( this.y, 2 ) );
+  }
+
+  add( pt: Point ): Point{
+    return new Point( this.x + pt.x, this.y + pt.y );
+  }
+
+  clone(): Point{
+    return new Point( this.x, this.y );
+  }
+
+  copyFrom( sourcePoint: Point ): void{
+    this.x = sourcePoint.x;
+    this.y = sourcePoint.y;
+  }
+
+  static distance( pt1: Point, pt2: Point ): number{
+    let dis: Point = pt1.subTract( pt2 );
+    return dis.length;
+  }
+
+  equals( toCompare: Point ){
+    return this.x === toCompare.x && this.y === toCompare.y;
+  }
+
+  offset( x: number, y: number ): void{
+    this.x += x;
+    this.y += y;
+  }
+
+  static polar( len: number, angle: number ): Point{
+    return new Point( len * Math.cos( angle ), len * Math.sin( angle ) );
+  }
+
+  setTo( x: number, y: number ): void{
+    this.x = x;
+    this.y = y;
+  }
+
+  subTract( pt: Point ){
+    return new Point( this.x - pt.x, this.y - pt.y );
+  }
+
+  toString(){
+    return "(x=" + this.y + ", y=" + this.y + ")";
+  }
+}
