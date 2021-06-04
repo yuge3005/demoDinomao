5e23462d6460ddd8278794cfc7e0a7a8ca38ee89
Wayne <wayne@gamesmartltd.com>
2021-06-04 09:47:41 +0800

    app width and app height can only set once

--- src/basicUI/settings/GlobalSettings.ts
+++ src/basicUI/settings/GlobalSettings.ts
@@ -4,19 +4,35 @@
  * @Author: Wayne Yu
  * @Date: 2021-06-03 09:58:40
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-06-03 18:06:07
+ * @LastEditTime: 2021-06-04 09:43:51
  */
 import { StageOrientationMode } from './StageOrientationMode';
 import { StageScaleMode } from './StageScaleMode';
 
 export class GlobalSettings {
-  public appWidth: number = 1080;
-  public appHeight: number = 1920;
+  private _appWidth: number = 1080;
+  private appWidthSet: boolean = false;
+  public set appWidth( value: number ){
+    if( this.appWidthSet ) throw new Error( "app width can only set once." );
+    this._appWidth = value;
+    this.appWidthSet = true;
+  }
+
+  private _appHeight: number = 1920;
+  private appHeightSet: boolean = false;
+  public set appHeight( value: number ){
+    if( this.appHeightSet ) throw new Error( "app height can only set once." );
+    this._appHeight = value;
+    this.appHeightSet = true;
+  }
 
   public stageWidth: number = 0;
   public stageHeight: number = 0;
 
-  public scale: number = 1;
+  private _scale: number = 1;
+  public get scale(): number{
+    return this._scale;
+  }
 
   /* default StageScaleMode.SHOW_ALL */
   public scaleMode: string = StageScaleMode.SHOW_ALL;
@@ -28,17 +44,17 @@ export class GlobalSettings {
   resize(){
     if( this.screenMode == StageOrientationMode.DEFAULT ){
       if( this.scaleMode == StageScaleMode.SHOW_ALL ){
-        this.scale = Math.min( document.documentElement.clientWidth / this.appWidth, document.documentElement.clientHeight / this.appHeight );
-        this.stageWidth = this.appHeight;
-        this.stageHeight = this.appHeight;
+        this._scale = Math.min( document.documentElement.clientWidth / this._appWidth, document.documentElement.clientHeight / this._appHeight );
+        this.stageWidth = this._appHeight;
+        this.stageHeight = this._appHeight;
       }
     }
     else if( this.screenMode == StageOrientationMode.PORTRAIT ){
       if( this.scaleMode == StageScaleMode.FIT_WIDTH ){
         this.stageWidth = Math.min( document.documentElement.clientWidth, document.documentElement.clientHeight );
         let tempHeight: number = Math.max( document.documentElement.clientWidth, document.documentElement.clientHeight );
-        this.scale = this.stageWidth / this.appWidth;
-        this.stageHeight = tempHeight / this.scale;
+        this._scale = this.stageWidth / this._appWidth;
+        this.stageHeight = tempHeight / this._scale;
       }
     }
   }
