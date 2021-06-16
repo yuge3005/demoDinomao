e59451484cedb096210fa423051d5252d77b9643
Wayne <wayne@gamesmartltd.com>
2021-06-16 17:17:28 +0800

    trace

--- src/service/trace.ts
+++ src/service/trace.ts
@@ -5,15 +5,26 @@ import { HttpRequest } from './http-request';
  * @Author: Wayne Yu
  * @Date: 2021-06-16 10:05:55
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-06-16 10:43:36
+ * @LastEditTime: 2021-06-16 17:06:54
  */
 export class trace {
   public static log( str: any, type: any = "d" ){
     if( HttpRequest.platForm == "Android" ){
       try{
-        eval( "androidLogger.log(" + str + ")" );
+        if( typeof str === "string" ){
+          window.location.href = "js://webview?log=" + str;
+          eval( "androidLogger.log(str)" );
+        }
+        else{
+          window.location.href = "js://webview?log=" + JSON.stringify(str);
+          eval( "androidLogger.log(JSON.stringify(str))" );
+        }
       }
-      catch(e){}
+      catch(e){
+        console.log( str );
+        console.log( "console.error" );
+        window.location.href = "js://webview?log=console.error";
+      }
     }
     else{
       console.log( str );
