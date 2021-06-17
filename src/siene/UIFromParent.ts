9e5ba5c1599f66802058ae96d4700e3b92c595ec
LAPTOP-GIBGAT67\Thinkpad <wayne@gamesmartltd.com>
2021-06-17 10:56:39 +0800

    UIFromParent

--- src/siene/UIFromParent.ts
+++ src/siene/UIFromParent.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { TextureData } from '../basicUI/image/texture-data';
 
 @Component({
   template: ''
 })
-export class UIFromParent implements OnInit {
+export class UIFromParent implements OnInit, OnChanges{
 
-  @Input()textureData!: TextureData
+  @Input()textureData!: TextureData;
+  private inited: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
+    this.subUIinit();
+  }
+
+  subUIinit(){
+    if( !this.textureData ) return;
+
     this.initUI();
+
+    this.inited = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if( this.textureData && !this.inited ) this.subUIinit();
   }
 
   initUI(){
--- src/siene/video/control-bar/control-bar.component.ts
+++ src/siene/video/control-bar/control-bar.component.ts
@@ -6,7 +6,7 @@
  * @LastEditors: Wayne Yu
  * @LastEditTime: 2021-06-10 18:12:59
  */
-import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { UIFromParent } from '../../UIFromParent';
 import { BitmapData } from './../../../basicUI/image/bitmap-data';
 
@@ -15,7 +15,7 @@ import { BitmapData } from './../../../basicUI/image/bitmap-data';
   templateUrl: './control-bar.component.html',
   styleUrls: ['./control-bar.component.css']
 })
-export class ControlBarComponent extends UIFromParent implements OnDestroy, OnChanges{
+export class ControlBarComponent extends UIFromParent implements OnDestroy{
 
   @Input() productImg: string = '';
 
@@ -23,26 +23,14 @@ export class ControlBarComponent extends UIFromParent implements OnDestroy, OnCh
   objectImgFrame!: BitmapData;
   addCoin!: BitmapData;
 
-  private inited: boolean = false;
-
   constructor() {
     super();
   }
 
   initUI() {
-    if( !this.textureData ){
-
-    }
-
     this.controlBg = this.textureData.getTexture( "ingame_bottom_bg" );
     this.objectImgFrame = this.textureData.getTexture( "object_info", 32, 255 );
     this.addCoin = this.textureData.getTexture( "btn_charge", 35, 40 );
-
-    this.inited = true;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if( this.textureData && !this.inited ) this.initUI();
   }
 
   ngOnDestroy(): void {
