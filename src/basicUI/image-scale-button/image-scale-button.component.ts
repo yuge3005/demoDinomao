eaf3223b65d361bb0257cf3eaa627ad8a606e9dc
LAPTOP-GIBGAT67\Thinkpad <yuge3005@163.com>
2021-06-24 17:39:10 +0800

    scale button

--- src/basicUI/image-scale-button/image-scale-button.component.ts
+++ src/basicUI/image-scale-button/image-scale-button.component.ts
@@ -10,12 +10,37 @@ import { BitmapData } from '../image/bitmap-data';
 export class ImageScaleButtonComponent extends ImageButtonComponent {
 
   @Input() buttonIcon!: BitmapData;
+  iconStyle: string = '';
 
   constructor() { 
     super();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes)
+    if( this.imgData ) super.ngOnChanges( changes );
+    if( this.imgData && this.buttonIcon ){
+      this.iconStyle = `
+        background-image: url("${this.buttonIcon.url}");
+        width: ${this.buttonIcon.w}px;
+        height: ${this.buttonIcon.h}px;
+        background-position: -${this.buttonIcon.x}px -${this.buttonIcon.y}px;
+        left: ${this.imgData.w * 0.5 + this.buttonIcon.left}px;
+        top: ${this.imgData.h * 0.5 + this.buttonIcon.top}px;
+        margin-left: ${-this.buttonIcon.w * 0.5}px;
+        margin-top: ${-this.buttonIcon.h * 0.5}px;
+      `
+    }
+  }
+
+  onDown( event: Event ){
+    var btn: HTMLDivElement = event.currentTarget as HTMLDivElement;
+    var icon = btn.children[0];
+    icon.className = "imgIconZoomin";
+  }
+
+  onUp( event: Event ){
+    var btn: HTMLDivElement = event.currentTarget as HTMLDivElement;
+    var icon = btn.children[0];
+    icon.className = "imgIcon";
   }
 }
