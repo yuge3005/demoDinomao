a4aac2d2f66a488b16cf22b0f30657bbc48c7c57
yuge3005 <yuge3005@163.com>
2021-07-19 16:38:07 +0800

    popup text

--- src/pagePart/gameUI/popup-text-field/popup-text-field.component.ts
+++ src/pagePart/gameUI/popup-text-field/popup-text-field.component.ts
@@ -4,18 +4,20 @@
 * @Author: Wayne Yu
 * @Date: 2021-07-19 12:00:32
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-07-19 13:55:10
+ * @LastEditTime: 2021-07-19 16:35:11
 */
+import { ElementRef, ViewChild } from '@angular/core';
 import { Rectangle } from './../../../basicUI/geom/rectangle';
 import { TextData } from './../../../service/gameData/TextData';
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { TextFieldComponent } from './../../../basicUI/text-field/text-field.component';
 
 @Component({
   selector: 'app-popup-text-field',
   templateUrl: './popup-text-field.component.html',
   styleUrls: ['./popup-text-field.component.css']
 })
-export class PopupTextFieldComponent implements OnInit {
+export class PopupTextFieldComponent extends TextFieldComponent{
 
   @Input() textData!: TextData;
   @Input() text: string = "";
@@ -23,11 +25,17 @@ export class PopupTextFieldComponent implements OnInit {
   txtRect: Rectangle = new Rectangle( 0, 0, 100, 20 );
   textAlign: string = "center";
   divStyle: string = "";
+  spanStyle: string = "";
   
   textStr: string = "";
-  colorStr: string = "#000000";
-  sizeStr: string = "10px";
+
+  color: number = 0;
+  size: number = 10;
   fontStr: string = "Arial";
+  stroke: number = 0;
+  strokeColor: number = 0;
+  
+  sizeStr: string = "10px";
   fontBold: string = "normal";
   fontStroke: string = "";
 
@@ -36,57 +44,56 @@ export class PopupTextFieldComponent implements OnInit {
   sizeOrTextChanged: boolean = false;
   multiViewCheck: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    super();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if( changes.textData ){
       var rect: any = this.textData.rect;
+      this.positionRect = new Rectangle( rect.x, rect.y, rect.w, rect.h );
       if( this.textData.align ) this.textAlign = this.textData.align;
+      this.updateDivStyle( this.positionRect, this.textAlign );
 
-      this.divStyle = `
-        top: ${rect.y}px;
-        left: ${rect.x}px;
-        width: ${rect.w}px;
-        height: ${rect.h}px;
-        line-height: ${rect.h}px;
-        text-align: ${this.textAlign};
+      if( this.textData.size ) this.size = this.textData.size;
+      if( this.textData.color ) this.color = this.textData.color;
+      if( this.textData.font ) this.fontStr = this.textData.font;
+      if( this.textData.stroke ){
+        this.stroke = this.textData.stroke;
+        this.strokeColor = this.textData.strokeColor;
+      }
+
+      this.spanStyle = `
+        font-family: ${this.fontStr};
+        font-weight: bold;
+        color: #FFFFFF;
+        font-size: 50px;
+        stroke: fontStroke;
+        text-stroke: 1px #f00;
+        -webkit-text-stroke: 1px #f00;
       `
-
-      if( price.color ) this.priceTextColor = price.color;
-    if( price.size ) this.priceTextSize = price.size;
-    if( price.font ) this.priceTextFont = price.font;
-    if( price.stroke ) this.priceStroke = price.stroke;
-    if( price.strokeColor ) this.priceStrokeColor = price.strokeColor;
-    }
-    if( changes.text ){
-      this.textStr = changes.text.currentValue;
-      this.sizeStr = this.size + "px";
-      this.sizeOrTextChanged = true;
-    }
-    if( changes.color ){
-      this.colorStr = "#" + this.toString16( this.color );
-    }
-    if( changes.size ){
-      this.sizeStr = this.size + "px";
-      this.sizeOrTextChanged = true;
-    }
-    if( changes.font ){
-      this.fontStr = this.font;
-    }
-    if( changes.bold ){
-      this.fontBold = this.bold ? "bold" : "normal";
-    }
-    if( changes.align ){
-      this.textAlign = this.align;
-    }
-    if( changes.stroke || changes.strokeColor ){
-      this.fontStroke = this.stroke ?  this.stroke + "px #" + this.toString16( this.strokeColor ) : "";
     }
   }
 
+  protected updateDivStyle( rect: Rectangle, align: string ){
+    if( !this.positionRect ) return;
+    this.divStyle = `
+      top: ${rect.y}px;
+      left: ${rect.x}px;
+      width: ${rect.width}px;
+      height: ${rect.height}px;
+      line-height: ${rect.height}px;
+      text-align: ${align};
+    `;
+  }
+
+  toString16( num: number ): string{
+    let numStr: string = num.toString( 16 );
+    let needAddZero: number = 6 - numStr.length;
+    let addArr: string[] = ["","0","00","000","0000","00000"];
+    return addArr[needAddZero] + numStr;
+  }
+  
   ngAfterViewChecked(){
     if( this.sp && this.sizeOrTextChanged ){
       if( this.multiViewCheck ) return;
