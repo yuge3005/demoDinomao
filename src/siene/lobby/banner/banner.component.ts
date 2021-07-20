74fd79a558e7b54c454c65ede6db483ece2e8807
yuge3005 <yuge3005@163.com>
2021-07-20 15:24:29 +0800

    carousel

--- src/siene/lobby/banner/banner.component.ts
+++ src/siene/lobby/banner/banner.component.ts
@@ -7,7 +7,7 @@ import { FirebaseAnaliyticsService } from './../../../service/firebase-analiytic
  * @Author: Wayne Yu
  * @Date: 2021-05-31 10:03:32
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-07-20 14:59:51
+ * @LastEditTime: 2021-07-20 15:23:22
  */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
@@ -20,6 +20,9 @@ export class BannerComponent implements OnInit, OnDestroy {
 
   private timerId: any;
   public featureData: FeatureVo[] = [{art:"assets/banner/banner_01.png"}];
+
+  carousel: string = "p0";
+  carouselCount: number = 0;
 
   constructor( private analytics: FirebaseAnaliyticsService ) { }
 
@@ -46,8 +49,9 @@ export class BannerComponent implements OnInit, OnDestroy {
       }, 5000);
     }
   }
-  
+
   loopFeature(){
-    
+    this.carouselCount++;
+    this.carousel = "p" + ++this.carouselCount % this.featureData.length;
   }
 }
