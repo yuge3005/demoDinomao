f9f2008e852247d13119227522f863c919006bec
yuge3005 <yuge3005@163.com>
2021-07-23 10:31:01 +0800

    Revert "try fix cross domain"
    This reverts commit 136f82cd5d383eacc2262be5735f52e8ed96cf09.

--- src/basicUI/ui/UIComponent.ts
+++ src/basicUI/ui/UIComponent.ts
@@ -1,12 +1,10 @@
-import { trace } from './../../service/gameUILogic/trace';
-import { HttpParams } from '@angular/common/http';
 /*
  * @Description:
  * @version: 1.0
  * @Author: Wayne Yu
  * @Date: 2021-05-27 13:34:15
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-07-23 10:09:37
+ * @LastEditTime: 2021-07-14 14:58:57
  */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -21,38 +19,15 @@ export class UIComponent implements OnInit, OnDestroy{
   textureJson: any;
   textureUrl: string = "";
 
-  xhr!: XMLHttpRequest;
-
   constructor( protected http: HttpClient ) { }
 
-  ngOnInit() {
-    // let param: any = new HttpParams().set( "Accept", "*/*" );
-    // this.textureJson = await this.http.get(this.textureUrl, param).toPromise();
+  async ngOnInit() {
+    this.textureJson = await this.http.get(this.textureUrl).toPromise();
 
-    // this.textureData = new TextureData();
-    // this.textureData.setFile( this.textureUrl.substr( 0, this.textureUrl.lastIndexOf( "/" ) + 1 ) + this.textureJson.file, this.textureJson.frames );
+    this.textureData = new TextureData();
+    this.textureData.setFile( this.textureUrl.substr( 0, this.textureUrl.lastIndexOf( "/" ) + 1 ) + this.textureJson.file, this.textureJson.frames );
 
-    // this.initUI();
-
-    this.xhr = new XMLHttpRequest();
-    this.xhr.open("GET", this.textureUrl, true);
-    this.xhr.addEventListener("load", this.assetsLoaded.bind( this ) );
-    this.xhr.setRequestHeader("Accept", "*/*");
-    this.xhr.send();
-  }
-
-  assetsLoaded( ev: ProgressEvent<XMLHttpRequestEventTarget> ){
-    this.xhr.removeEventListener("load", this.assetsLoaded.bind( this ) );
-    let str: string = this.xhr.response;
-    try{
-      this.textureJson = JSON.parse( str )
-      this.textureData = new TextureData();
-      this.textureData.setFile( this.textureUrl.substr( 0, this.textureUrl.lastIndexOf( "/" ) + 1 ) + this.textureJson.file, this.textureJson.frames );
-      this.initUI();
-    }
-    catch(e){
-      trace.log( "popup assets missing" );
-    }
+    this.initUI();
   }
 
   initUI(){
