d15c0a69f1796fce3f77a758e0b091ea1e01d6a6
yuge3005 <yuge3005@163.com>
2021-08-26 11:28:59 +0800

    check ui

--- src/popups/daily-bonus/daily-item/daily-item.component.ts
+++ src/popups/daily-bonus/daily-item/daily-item.component.ts
@@ -6,7 +6,7 @@ import { TextData, DailyBonus } from './../../../service/dinomao-game.module';
  * @Author: Wayne Yu
  * @Date: 2021-08-25 16:44:20
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-08-26 10:55:12
+ * @LastEditTime: 2021-08-26 11:28:11
  */
 import { Component, Input, SimpleChanges } from '@angular/core';
 
@@ -19,6 +19,7 @@ export class DailyItemComponent extends UIFromParent {
 
   dailyItemBg!: BitmapData;
   coinIcon!: BitmapData;
+  greenCheck!: BitmapData;
   titleText!: TextData;
   coinText!: TextData;
   position: Point = new Point;
@@ -26,7 +27,15 @@ export class DailyItemComponent extends UIFromParent {
   @Input() itemData!: any;
   @Input() index: number = -1;
   iNumber: number = 0;
-  dailyCoin: number = 0;
+  isToday: boolean = false;
+  
+  get showHand(): boolean{
+    return this.isToday && !DailyBonus.instance.hasDailyBonus;
+  }
+
+  get showMask(): boolean{
+    return ( this.iNumber < DailyBonus.instance.daysRow ) || ( this.isToday && DailyBonus.instance.hasDailyBonus )
+  }
 
   constructor() {
     super();
@@ -34,15 +43,15 @@ export class DailyItemComponent extends UIFromParent {
 
   initUI() {
     this.iNumber = this.index + 1;
-    this.dailyItemBg = this.textureData.getTexture( DailyBonus.instance.daysRow == this.iNumber ? "bg1" : "bg" );
+    this.isToday = DailyBonus.instance.daysRow == this.iNumber;
+    this.dailyItemBg = this.textureData.getTexture( this.isToday ? "bg1" : "bg" );
     let coinLevel: number = DailyBonus.instance.bonusLevel.indexOf( this.itemData ) + 1;
     coinLevel = Math.min( 4, coinLevel );
     this.coinIcon = this.textureData.getTexture( "gold_" + coinLevel, 24, 105 );
+    this.greenCheck = this.textureData.getTexture( "Check sign", 14, 54 );
 
     this.titleText = {"color":0x81665a,"strokeColor":0,"rect":{"h":60,"y":10,"w":204,"x":0},"font":"arial","stroke":0,"size":35,"align":"center"};
     this.coinText = {"color":0xffc20f,"strokeColor":0xff3f3b,"rect":{"h":80,"y":105,"w":204,"x":125},"font":"FRAHV_0","stroke":3,"size":40,"align":"left"};
-
-    this.dailyCoin = this.itemData;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
