84bf69c0fdb3dac8533f1b27127120588bd4c90f
yuge3005 <yuge3005@163.com>
2021-08-26 10:29:06 +0800

    daily bonus ui

--- src/popups/daily-bonus/daily-item/daily-item.component.ts
+++ src/popups/daily-bonus/daily-item/daily-item.component.ts
@@ -1,11 +1,12 @@
 import { UIFromParent, Point, BitmapData } from './../../../basicUI/basic-ui.module';
+import { TextData } from './../../../service/dinomao-game.module';
 /*
  * @Description: 
  * @version: 1.0
  * @Author: Wayne Yu
  * @Date: 2021-08-25 16:44:20
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-08-25 17:14:48
+ * @LastEditTime: 2021-08-26 10:27:31
  */
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { DailyBonus } from 'src/service/user/DailyBonus';
@@ -18,18 +19,29 @@ import { DailyBonus } from 'src/service/user/DailyBonus';
 export class DailyItemComponent extends UIFromParent {
 
   dailyItemBg!: BitmapData;
+  coinIcon!: BitmapData;
+  titleText!: TextData;
+  coinText!: TextData;
   position: Point = new Point;
 
   @Input() itemData!: any;
   @Input() index: number = -1;
+  iNumber: number = 0;
+  dailyCoin: number = 0;
 
   constructor() {
     super();
   }
 
   initUI() {
-    var iNumber: number = this.index + 1;
-    this.dailyItemBg = this.textureData.getTexture( DailyBonus.instance.daysRow == iNumber ? "bg1" : "bg" );
+    this.iNumber = this.index + 1;
+    this.dailyItemBg = this.textureData.getTexture( DailyBonus.instance.daysRow == this.iNumber ? "bg1" : "bg" );
+    this.coinIcon = this.textureData.getTexture( "gold_1", 24, 105 );
+
+    this.titleText = {"color":0x81665a,"strokeColor":0,"rect":{"h":60,"y":10,"w":204,"x":0},"font":"arial","stroke":0,"size":35,"align":"center"};
+    this.coinText = {"color":0xffc20f,"strokeColor":0xff3f3b,"rect":{"h":80,"y":105,"w":204,"x":125},"font":"FRAHV_0","stroke":3,"size":40,"align":"left"};
+
+    this.dailyCoin = this.itemData;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
