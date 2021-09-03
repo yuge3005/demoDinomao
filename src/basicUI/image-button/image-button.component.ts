69cf27ecd716aa79735367d5c79e34d1df48847a
yuge3005 <yuge3005@163.com>
2021-09-03 14:16:45 +0800

    image button add point event

--- src/basicUI/image-button/image-button.component.ts
+++ src/basicUI/image-button/image-button.component.ts
@@ -1,12 +1,13 @@
 /*
- * @Description: 
- * @version: 1.0
- * @Author: Wayne Yu
- * @Date: 2021-06-29 14:45:12
+* @Description: 
+* @version: 1.0
+* @Author: Wayne Yu
+* @Date: 2021-06-29 14:45:12
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-07-14 14:12:50
- */
+ * @LastEditTime: 2021-09-03 14:02:23
+*/
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Point } from '../geom/point';
 import { ImageComponent } from '../image/image.component';
 
 @Component({
@@ -16,13 +17,14 @@ import { ImageComponent } from '../image/image.component';
 })
 export class ImageButtonComponent extends ImageComponent {
 
-  @Output() itemClick: EventEmitter<any> = new EventEmitter<any>();
+  @Output() itemClick: EventEmitter<Point> = new EventEmitter<Point>();
 
   constructor() { 
     super();
   }
 
-  onButtonClick(){
-    this.itemClick.emit();
+  onButtonClick( event: any ){
+    let pt: Point = new Point().init( event.offsetX, event.offsetY );
+    this.itemClick.emit( pt );
   }
 }
