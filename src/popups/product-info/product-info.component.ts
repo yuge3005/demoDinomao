f0f60374021a2f6510cde28a99b88ca94bb156b0
yuge3005 <yuge3005@163.com>
2021-09-10 11:07:39 +0800

    picture in product info

--- src/popups/product-info/product-info.component.ts
+++ src/popups/product-info/product-info.component.ts
@@ -1,16 +1,15 @@
-import { TextData } from './../../service/gameData/TextData';
 /*
 * @Description: 
 * @version: 1.0
 * @Author: Wayne Yu
 * @Date: 2021-09-09 16:53:48
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-09-09 17:56:38
+ * @LastEditTime: 2021-09-10 11:05:21
 */
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BitmapData } from '../../basicUI/basic-ui.module';
-import { GenericModalComponent } from '../../service/ui/generic-modal.component';
+import { GenericModalComponent, TextData, Trigger } from '../../service/dinomao-game.module';
 
 @Component({
   selector: 'app-product-info',
@@ -27,6 +26,13 @@ export class ProductInfoComponent extends GenericModalComponent{
   nameString: string = "";
   tipString: string = "";
 
+  productImg: string = "";
+
+  isBgLoaded: boolean = false;
+  isProductLoaded: boolean = false;
+
+  isVip: boolean = false;
+
   constructor(public http: HttpClient) {
     super( http );
   }
@@ -37,9 +43,29 @@ export class ProductInfoComponent extends GenericModalComponent{
     this.goBtn = this.buildUI( this.textureJson.goBtn );
     this.closeBtn = this.buildUI( this.textureJson.closeBtn );
     this.vip = this.buildUI( this.textureJson.vip );
+
+    let products: any = Trigger.popupData.products;
+    let product: any = products[0];
+    let data: any = product.product;
+    this.productImg = data.img;
+    this.isVip = data.isVIP == "1";
   }
 
   goPlay(){
     
+  }
+
+  bgTextureLoaded(){
+    this.isBgLoaded = true;
+    this.checkLoad();
+  }
+
+  productImgLoaded(){
+    this.isProductLoaded = true;
+    this.checkLoad();
+  }
+
+  checkLoad(){
+    if( this.isProductLoaded && this.isBgLoaded ) this.loaded = true;
   }
 }
