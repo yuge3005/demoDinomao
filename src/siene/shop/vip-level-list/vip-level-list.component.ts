4523cd41437067c961e2ec21b6486775ef32874f
yuge3005 <yuge3005@163.com>
2021-09-23 18:03:24 +0800

    vip level

--- src/siene/shop/vip-level-list/vip-level-list.component.ts
+++ src/siene/shop/vip-level-list/vip-level-list.component.ts
@@ -1,14 +1,14 @@
-import { BitmapData } from './../../../basicUI/image/bitmap-data';
 /*
 * @Description: 
 * @version: 1.0
 * @Author: Wayne Yu
 * @Date: 2021-09-23 15:22:50
- * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-09-23 16:12:51
+* @LastEditors: Wayne Yu
+* @LastEditTime: 2021-09-23 18:02:28
 */
 import { Component, Input } from '@angular/core';
-import { UIFromParent } from '../../../basicUI/basic-ui.module';
+import { UIFromParent, BitmapData } from '../../../basicUI/basic-ui.module';
+import { TextData, Trigger } from './../../../service/dinomao-game.module';
 
 @Component({
   selector: 'app-vip-level-list',
@@ -30,6 +30,23 @@ export class VipLevelListComponent extends UIFromParent {
 
   buyBtn!: BitmapData;
 
+  titleText!: TextData; 
+  tipText!: TextData;
+  priceText!: TextData;
+  titleString: string = "";
+  tipString: string = "";
+  priceString: string = "";
+
+  products?: any[];
+
+  vipLevel: number = 0;
+  get currentItem(): any{
+    if( !this.products ) return null;
+    let product: any = this.products[ this.vipLevel ];
+    let items: any = product.items;
+    return items;
+  }
+
   constructor() {
     super();
   }
@@ -45,6 +62,30 @@ export class VipLevelListComponent extends UIFromParent {
     this.hand = this.textureData.getTexture( "hand", 420, 157 );
 
     this.buyBtn = this.textureData.getTexture( "btn_subscribe", 188, 1050 );
+
+    this.titleText = {rect:{x:45,y:265,w:640,h:65},color:0xFFFFFF,size:60,font:"arialbk",align:"left",stroke:3,strokeColor:0x6d98e7};
+    this.tipText = {rect:{x:45,y:250,w:640,h:65},color:0xFFFFFF,size:45,font:"arialbk",align:"left",stroke:3,strokeColor:0x6d98e7};
+    this.priceText = {rect:{x:45,y:910,w:646,h:102},color:0xFFE635,size:90,font:"FRAHV_0",align:"center",stroke:6,strokeColor:0xAC1200,bold:true};
+    this.products = Trigger.vipData;
+    this.switchVip( this.vipLevel );
+  }
+
+  switchVip( vipLevel: number ){
+    this.vipLevel = vipLevel;
+    if( !this.products ) return;
+    this.priceString = "$ " + this.products[this.vipLevel].price + "/mo";
+    for( let i: number = 0; i < this.currentItem.length; i++ ){
+      if( this.currentItem[i].type == "item_subscription_name" ){
+        this.titleString = "Benefits of " + this.currentItem[i].item_subscription_name + " VIP：";
+      }
+      if( this.currentItem[i].type == "item_subscription_des" ){
+        this.tipString = this.currentItem[i].item_subscription_des;
+        this.tipString = this.tipString.replace( /\<p\>/g, "" );
+        this.tipString = this.tipString.replace( /\<\/p\>/g, "" );
+        this.tipString = this.tipString.replace( /\*/g, "<br/>" );
+        document.getElementsByClassName( "tipText" )[0].innerHTML = this.tipString;
+      }
+    }
   }
 
   buyVip(){
