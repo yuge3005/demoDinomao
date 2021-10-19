13f51d15515911696ee58013a3196c45e2461cba
yuge3005 <yuge3005@163.com>
2021-10-19 11:18:26 +0800

    add easing

--- src/basicUI/tween/Ease.ts
+++ src/basicUI/tween/Ease.ts
@@ -4,8 +4,21 @@
  * @Author: Wayne Yu
  * @Date: 2021-10-18 16:00:45
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-10-19 09:43:38
+ * @LastEditTime: 2021-10-19 11:16:35
  */
 export class Ease {
     public static Linear: string = "linear";
+    public static RegularEaseOut: string = "";
+    public static BackEaseIn: string = "BackEaseIn";
+    public static BackEaseOut: string = "BackEaseOut";
+    public static BackEaseInOut: string = "BackEaseInOut";
+    public static BounceEaseIn: string = "BounceEaseIn";
+    public static BounceEaseOut: string = "BounceEaseOut";
+    public static BounceEaseInOut: string = "BounceEaseInOut";
+    public static CircEaseIn: string = "CircEaseIn";
+    public static CircEaseOut: string = "CircEaseOut";
+    public static CircEaseInOut: string = "CircEaseInOut";
+    public static CubicEaseIn: string = "CubicEaseIn";
+    public static CubicEaseOut: string = "CubicEaseOut";
+    public static CubicEaseInOut: string = "CubicEaseInOut";
 }
--- src/basicUI/tween/Easing.ts
+++ src/basicUI/tween/Easing.ts
@@ -5,26 +5,92 @@ import { Ease } from './Ease';
  * @Author: Wayne Yu
  * @Date: 2021-10-19 09:42:55
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-10-19 10:39:58
+ * @LastEditTime: 2021-10-19 11:17:07
  */
 export class Easing {
 
     public static easing( easeKey: string ): Function{
         let func: Function;
         switch( easeKey ){
-            case Ease.Linear: func = this.Linear;
-                break;
-            default: func = this.RegularEaseOut;
-                break;
+            case Ease.Linear: func = this.Linear; break;
+            case Ease.BackEaseIn: func = this.BackEaseIn; break;
+            case Ease.BackEaseOut: func = this.BackEaseOut; break;
+            case Ease.BackEaseInOut: func = this.BackEaseInOut; break;
+            case Ease.BounceEaseIn: func = this.BounceEaseIn; break;
+            case Ease.BounceEaseOut: func = this.BounceEaseOut; break;
+            case Ease.BounceEaseInOut: func = this.BounceEaseInOut; break;
+            case Ease.CircEaseIn: func = this.CircEaseIn; break;
+            case Ease.CircEaseOut: func = this.CircEaseOut; break;
+            case Ease.CircEaseInOut: func = this.CircEaseInOut; break;
+            case Ease.CubicEaseIn: func = this.CubicEaseIn; break;
+            case Ease.CubicEaseOut: func = this.CubicEaseOut; break;
+            case Ease.CubicEaseInOut: func = this.CubicEaseInOut; break;
+            default: func = this.RegularEaseOut; break;
         }
         return func;
     }
 
-    public static Linear: Function = function easeNone (t:number, b:number, c:number, d:number):number {
+    public static Linear (t:number, b:number, c:number, d:number):number {
         return c*t/d + b;
     }
 
-    public static RegularEaseOut: Function = function easeNone (t:number, b:number, c:number, d:number):number {
+    public static RegularEaseOut (t:number, b:number, c:number, d:number):number {
         return 1 - (t = 1 - (t / d)) * t;
+    }
+
+    public static BackEaseIn (t:number, b:number, c:number, d:number, s:number = 1.70158):number {
+        return c*(t/=d)*t*((s+1)*t - s) + b;
+    }
+
+    public static BackEaseOut (t:number, b:number, c:number, d:number, s:number = 1.70158):number {
+        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+    }
+
+    public static BackEaseInOut (t:number, b:number, c:number, d:number, s:number = 1.70158):number {
+        if ((t/=d*0.5) < 1) return c*0.5*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+        return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+    }
+
+    public static BounceEaseOut (t:number, b:number, c:number, d:number):number {
+        if ((t/=d) < (1/2.75)) {
+            return c*(7.5625*t*t) + b;
+        } else if (t < (2/2.75)) {
+            return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+        } else if (t < (2.5/2.75)) {
+            return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+        } else {
+            return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+        }
+    }
+
+    public static BounceEaseIn (t:number, b:number, c:number, d:number):number {
+        return c - this.BounceEaseOut(d-t, 0, c, d) + b;
+    }
+    
+    public static BounceEaseInOut (t:number, b:number, c:number, d:number):number {
+        if (t < d*0.5) return this.BounceEaseIn (t*2, 0, c, d) * .5 + b;
+        else return this.BounceEaseOut (t*2-d, 0, c, d) * .5 + c*.5 + b;
+    }
+
+    public static CircEaseIn (t:number, b:number, c:number, d:number):number {
+        return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+    }
+    public static CircEaseOut (t:number, b:number, c:number, d:number):number {
+        return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+    }
+    public static CircEaseInOut (t:number, b:number, c:number, d:number):number {
+        if ((t/=d*0.5) < 1) return -c*0.5 * (Math.sqrt(1 - t*t) - 1) + b;
+        return c*0.5 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+    }
+		
+    public static CubicEaseIn (t:number, b:number, c:number, d:number):number {
+        return c*(t/=d)*t*t + b;
+    }
+    public static CubicEaseOut (t:number, b:number, c:number, d:number):number {
+        return c*((t=t/d-1)*t*t + 1) + b;
+    }
+    public static CubicEaseInOut (t:number, b:number, c:number, d:number):number {
+        if ((t/=d*0.5) < 1) return c*0.5*t*t*t + b;
+        return c*0.5*((t-=2)*t*t + 2) + b;
     }
 }
