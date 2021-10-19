1ba0543bf0031592f71c148c86e73530e509072e
yuge3005 <yuge3005@163.com>
2021-10-19 11:29:54 +0800

    add easing

--- src/basicUI/tween/Ease.ts
+++ src/basicUI/tween/Ease.ts
@@ -4,7 +4,7 @@
  * @Author: Wayne Yu
  * @Date: 2021-10-18 16:00:45
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-10-19 11:16:35
+ * @LastEditTime: 2021-10-19 11:26:02
  */
 export class Ease {
     public static Linear: string = "linear";
@@ -21,4 +21,10 @@ export class Ease {
     public static CubicEaseIn: string = "CubicEaseIn";
     public static CubicEaseOut: string = "CubicEaseOut";
     public static CubicEaseInOut: string = "CubicEaseInOut";
+    public static ElasticEaseIn: string = "ElasticEaseIn";
+    public static ElasticEaseOut: string = "ElasticEaseOut";
+    public static ElasticEaseInOut: string = "ElasticEaseInOut";
+    public static ExpoEaseIn: string = "ExpoEaseIn";
+    public static ExpoEaseOut: string = "ExpoEaseOut";
+    public static ExpoEaseInOut: string = "ExpoEaseInOut";
 }
--- src/basicUI/tween/Easing.ts
+++ src/basicUI/tween/Easing.ts
@@ -5,9 +5,11 @@ import { Ease } from './Ease';
  * @Author: Wayne Yu
  * @Date: 2021-10-19 09:42:55
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-10-19 11:17:07
+ * @LastEditTime: 2021-10-19 11:27:55
  */
 export class Easing {
+    
+    private static _2PI:number = Math.PI * 2;
 
     public static easing( easeKey: string ): Function{
         let func: Function;
@@ -25,6 +27,12 @@ export class Easing {
             case Ease.CubicEaseIn: func = this.CubicEaseIn; break;
             case Ease.CubicEaseOut: func = this.CubicEaseOut; break;
             case Ease.CubicEaseInOut: func = this.CubicEaseInOut; break;
+            case Ease.ElasticEaseIn: func = this.ElasticEaseIn; break;
+            case Ease.ElasticEaseOut: func = this.ElasticEaseOut; break;
+            case Ease.ElasticEaseInOut: func = this.ElasticEaseInOut; break;
+            case Ease.ExpoEaseIn: func = this.ExpoEaseIn; break;
+            case Ease.ExpoEaseOut: func = this.ExpoEaseOut; break;
+            case Ease.ExpoEaseInOut: func = this.ExpoEaseInOut; break;
             default: func = this.RegularEaseOut; break;
         }
         return func;
@@ -75,9 +83,11 @@ export class Easing {
     public static CircEaseIn (t:number, b:number, c:number, d:number):number {
         return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
     }
+
     public static CircEaseOut (t:number, b:number, c:number, d:number):number {
         return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
     }
+
     public static CircEaseInOut (t:number, b:number, c:number, d:number):number {
         if ((t/=d*0.5) < 1) return -c*0.5 * (Math.sqrt(1 - t*t) - 1) + b;
         return c*0.5 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
@@ -86,11 +96,53 @@ export class Easing {
     public static CubicEaseIn (t:number, b:number, c:number, d:number):number {
         return c*(t/=d)*t*t + b;
     }
+
     public static CubicEaseOut (t:number, b:number, c:number, d:number):number {
         return c*((t=t/d-1)*t*t + 1) + b;
     }
+    
     public static CubicEaseInOut (t:number, b:number, c:number, d:number):number {
         if ((t/=d*0.5) < 1) return c*0.5*t*t*t + b;
         return c*0.5*((t-=2)*t*t + 2) + b;
+    }
+		
+    public static ElasticEaseIn (t:number, b:number, c:number, d:number, a:number = 0, p:number = 0):number {
+        var s:number;
+        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+        if (!a || (c > 0 && a < c) || (c < 0 && a < -c)) { a=c; s = p/4; }
+        else s = p/this._2PI * Math.asin (c/a);
+        return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*this._2PI/p )) + b;
+    }
+
+    public static ElasticEaseOut (t:number, b:number, c:number, d:number, a:number = 0, p:number = 0):number {
+        var s:number;
+        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+        if (!a || (c > 0 && a < c) || (c < 0 && a < -c)) { a=c; s = p/4; }
+        else s = p/this._2PI * Math.asin (c/a);
+        return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*this._2PI/p ) + c + b);
+    }
+
+    public static ElasticEaseInOut (t:number, b:number, c:number, d:number, a:number = 0, p:number = 0):number {
+        var s:number;
+        if (t==0) return b;  if ((t/=d*0.5)==2) return b+c;  if (!p) p=d*(.3*1.5);
+        if (!a || (c > 0 && a < c) || (c < 0 && a < -c)) { a=c; s = p/4; }
+        else s = p/this._2PI * Math.asin (c/a);
+        if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*this._2PI/p )) + b;
+        return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*this._2PI/p )*.5 + c + b;
+    }
+
+    public static ExpoEaseIn(t:number, b:number, c:number, d:number):number {
+        return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b - c * 0.001;
+    }
+
+    public static ExpoEaseOut(t:number, b:number, c:number, d:number):number {
+        return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+    }
+
+    public static ExpoEaseInOut(t:number, b:number, c:number, d:number):number {
+        if (t==0) return b;
+        if (t==d) return b+c;
+        if ((t/=d*0.5) < 1) return c*0.5 * Math.pow(2, 10 * (t - 1)) + b;
+        return c*0.5 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 }
