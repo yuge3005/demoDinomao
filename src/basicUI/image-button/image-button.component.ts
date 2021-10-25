e8c84d2d2428dce1cbe273349864d5d096481800
yuge3005 <yuge3005@163.com>
2021-10-25 17:56:59 +0800

    sound

--- src/basicUI/image-button/image-button.component.ts
+++ src/basicUI/image-button/image-button.component.ts
@@ -4,9 +4,9 @@
 * @Author: Wayne Yu
 * @Date: 2021-06-29 14:45:12
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-09-13 17:26:10
+ * @LastEditTime: 2021-10-25 17:39:52
 */
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { Point } from '../geom/point';
 import { ImageComponent } from '../image/image.component';
 import { SoundManager } from '../sound/SoundManager';
@@ -19,6 +19,7 @@ import { SoundManager } from '../sound/SoundManager';
 export class ImageButtonComponent extends ImageComponent {
 
   @Output() itemClick: EventEmitter<Point> = new EventEmitter<Point>();
+  @Input() soundUrl: string = "";
 
   constructor() {
     super();
@@ -28,6 +29,7 @@ export class ImageButtonComponent extends ImageComponent {
     let pt: Point = new Point().init( event.offsetX, event.offsetY );
     this.itemClick.emit( pt );
 
-    if( SoundManager.defaltButtonSound ) SoundManager.play( SoundManager.defaltButtonSound );
+    if( this.soundUrl ) SoundManager.play( this.soundUrl );
+    else if( SoundManager.defaltButtonSound ) SoundManager.play( SoundManager.defaltButtonSound );
   }
 }
