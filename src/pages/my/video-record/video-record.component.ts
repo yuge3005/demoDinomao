a81b7d400f46c3ad298fd024016354ef310648d8
yuge3005 <yuge3005@163.com>
2021-10-27 16:35:16 +0800

    get list data

--- src/pages/my/video-record/video-record.component.ts
+++ src/pages/my/video-record/video-record.component.ts
@@ -4,12 +4,12 @@
  * @Author: Wayne Yu
  * @Date: 2021-10-27 15:37:52
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-10-27 15:58:50
+ * @LastEditTime: 2021-10-27 16:29:52
  */
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BitmapData } from '../../../basicUI/basic-ui.module';
-import { MainPage, Trigger, WebPages, Loading, TextData, GM } from '../../../service/dinomao-game.module';
+import { MainPage, Trigger, WebPages, Loading, HttpRequest, GM } from '../../../service/dinomao-game.module';
 
 @Component({
   selector: 'app-video-record',
@@ -29,11 +29,17 @@ export class VideoRecordComponent extends MainPage {
   initUI() {
     Loading.status = 2;
 
-    this.backBtn = this.textureData.getTexture( "btn_return", 30, 135 );
-    this.title = this.textureData.getTexture( "VIDEOS", 294, 137 );
+    this.backBtn = this.textureData.getTexture( "btn_return", 30, 125 );
+    this.title = this.textureData.getTexture( "VIDEOS", 294, 150 );
+
+    new HttpRequest().loadData( "apis/v1/user/videos?" + GM.interfaceString + "&weeks=3&created=desc", this.getRecordList.bind(this), "GET", "" );
   }
 
   gotoBack(){
     Trigger.gotoPage( WebPages.USER_CENTER );
+  }
+
+  getRecordList( data: any ){
+    console.log( data );
   }
 }
