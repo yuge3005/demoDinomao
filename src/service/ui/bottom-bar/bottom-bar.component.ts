7ecd4ed5772b10c353ef6be4744bb6a6f1def6a5
yuge3005 <yuge3005@163.com>
2021-11-09 16:15:27 +0800

    bottom bar icons

--- src/service/ui/bottom-bar/bottom-bar.component.ts
+++ src/service/ui/bottom-bar/bottom-bar.component.ts
@@ -6,7 +6,7 @@ import { Trigger } from '../../gameUILogic/Trigger';
  * @Author: Wayne Yu
  * @Date: 2021-05-31 12:56:52
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-11-02 10:33:53
+ * @LastEditTime: 2021-11-09 16:13:41
  */
 import { Component } from '@angular/core';
 import { UIComponent, BitmapData } from '../../../basicUI/basic-ui.module';
@@ -21,9 +21,12 @@ export class BottomBarComponent extends UIComponent{
   bottomBg!: BitmapData;
   home!: BitmapData;
   shop!: BitmapData;
-  rank!: BitmapData;
+  price!: BitmapData;
   my!: BitmapData;
-
+  homeIcon!: BitmapData;
+  shopIcon!: BitmapData;
+  priceIcon!: BitmapData;
+  myIcon!: BitmapData;
   constructor() {
     super();
     this.textureUrl = "assets/bottom_bar/bottom_bar.json";
@@ -31,10 +34,14 @@ export class BottomBarComponent extends UIComponent{
 
    initUI(){
     this.bottomBg = this.textureData.getTexture( "bottom_bg" );
-    this.home = this.textureData.getTexture( "HOME", 30, 10 );
-    this.shop = this.textureData.getTexture( "SHOP", 226, 10 );
-    this.rank = this.textureData.getTexture( "RANK", 422, 10 );
-    this.my = this.textureData.getTexture( "MY", 618, 10 );
+    this.home = this.textureData.getTexture( "HOME", 44, 30 );
+    this.shop = this.textureData.getTexture( "SHOP", 244, 32 );
+    this.price = this.textureData.getTexture( "PRIZE", 444, 30 );
+    this.my = this.textureData.getTexture( "MY", 627, 30 );
+    this.homeIcon = this.textureData.getTexture( "HOME1", 30, 5 );
+    this.shopIcon = this.textureData.getTexture( "SHOP1", 220, 14 );
+    this.priceIcon = this.textureData.getTexture( "PRIZE1", 422, 5 );
+    this.myIcon = this.textureData.getTexture( "MY1", 618, 5 );
   }
 
   gotoBank(){
