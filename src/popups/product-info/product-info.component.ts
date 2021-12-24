921a09e90cdb23023ac4d526c7af66f69eee1603
yuge3005 <yuge3005@163.com>
2021-12-24 10:50:50 +0800

    delete testing movieclip

--- src/popups/product-info/product-info.component.ts
+++ src/popups/product-info/product-info.component.ts
@@ -1,15 +1,14 @@
-import { BigDice } from './BigDice';
 /*
 * @Description: 
 * @version: 1.0
 * @Author: Wayne Yu
 * @Date: 2021-09-09 16:53:48
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-12-16 15:10:02
+ * @LastEditTime: 2021-12-24 10:02:26
 */
 import { Component } from '@angular/core';
-import { BitmapData, MovieClip, SimpleMovieClip, MovieClipDataFactory, Ease, Tween, Point } from '../../basicUI/basic-ui.module';
-import { GenericModalComponent, TextData, Trigger, GoodsData } from '../../service/dinomao-game.module';
+import { BitmapData, SimpleMovieClip } from '../../basicUI/basic-ui.module';
+import { GenericModalComponent, TextData, Trigger, GoodsData, WebPages } from '../../service/dinomao-game.module';
 
 @Component({
   selector: 'app-product-info',
@@ -36,10 +35,6 @@ export class ProductInfoComponent extends GenericModalComponent{
   itemData!: GoodsData;
 
   guang!: SimpleMovieClip;
-  fireworks!: SimpleMovieClip;
-  turbo90!: MovieClip;
-  turboBalls!: MovieClip;
-  dice!: BigDice;
 
   constructor() {
     super();
@@ -66,43 +61,11 @@ export class ProductInfoComponent extends GenericModalComponent{
     this.guang = new SimpleMovieClip( "assets/guangAnimation/guang_tex.png", "assets/guangAnimation/guang_tex.json" );
     this.guang.scaleX = this.guang.scaleY = 1.4;
     this.guang.setPosition( 190, 410 );
-
-    this.fireworks = new SimpleMovieClip( "assets/common_popups/result/fireworks.png", "assets/common_popups/result/fireworks.json" );
-    this.fireworks.scaleX = this.fireworks.scaleY = 2.4;
-    this.fireworks.setPosition( 180, 200 );
-
-    let mcf: MovieClipDataFactory = new MovieClipDataFactory( "assets/mc/turbo90Animation.png", "assets/mc/turbo90Animation.json" );
-    this.turbo90 = new MovieClip( mcf.getMovieClipData( "lotto_balls_up" ) );
-    this.turbo90.setPosition( 150 + 225, 0 );
-    this.turbo90.setAnchorOffset( 225, 0 );
-    this.turbo90.scaleX = this.turbo90.scaleY = 0.5;
-
-    this.turboBalls = new MovieClip( mcf.getMovieClipData("lotto_balls") );
-    this.turboBalls.setPosition( 150 + 225, 0 );
-    this.turboBalls.setAnchorOffset( 225, 0 );
-    this.turboBalls.scaleX = this.turboBalls.scaleY = 0.5;
-
-    this.dice = new BigDice();
-    this.dice.setPosition( 200, 300 );
-    this.dice.scaleX = this.dice.scaleY = 3;
-    this.dice.gotoAndStop( "d17" );
   }
 
   goPlay(){
-    // this.closePo();
-    // Trigger.gotoPage( WebPages.VIDEO, this.itemData );
-    this.dice.setPosition( 550, 200 );
-    this.dice.startPosition = new Point().init( 550, 200 );
-
-    this.dice.endPosition = new Point().init( 100 + Math.random() * 200, 500 );
-    this.dice.middlePosition = new Point().init( 400 + Math.random() * 100, 500 );
-
-    this.dice.gotoAndPlay( "loop" );
-    Tween.to( this.dice, 1, { factor: 1 }, 0, this.getNumber.bind( this ), Ease.CircEaseInOut );
-  }
-  
-  getNumber(){
-    this.dice.gotoAndStop( "d" + Math.floor( Math.random() * 6 + 16 ) );
+    this.closePo();
+    Trigger.gotoPage( WebPages.VIDEO, this.itemData );
   }
 
   bgTextureLoaded(){
