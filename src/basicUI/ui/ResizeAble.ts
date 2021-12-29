60816ef51c866f5ab69195ff38ae106d624d217c
yuge3005 <yuge3005@163.com>
2021-12-29 11:57:39 +0800

    notes

--- src/basicUI/ui/ResizeAble.ts
+++ src/basicUI/ui/ResizeAble.ts
@@ -1,27 +1,45 @@
-/*
- * @Description:
+/**
  * @version: 1.0
  * @Author: Wayne Yu
- * @Date: 2021-06-03 11:15:15
- * @LastEditors: Wayne Yu
  * @LastEditTime: 2021-10-22 15:38:38
+ * @description: Abstract component, must be extends.This component is used to adapt the size of the screen, which can be accessed through Application.settings.appWidth, and Application.settings.appHeight to set the design size of the application, and through Application.settings.screenMode,Application.settings.scalemode to set the zoom mode and screen rotation mode.
+ * @ In the component template, you need to use "[attr. Style] ='matrix'" to adaptive.don't override "ngOnInit" and "ngOnChanges".
+ * @ 抽象组件，必须继承。该组件用于自适应屏幕的尺寸，可通过Application.settings.appWidth,和Application.settings.appHeight来设定应用的设计尺寸,并通过Application.settings.screenMode,Application.settings.scaleMode来设置缩放模式和屏幕旋转模式.
+ * @ 在组件模板中，需使用"[attr.style]='matrix'"来实现自适应,不要覆盖ngOnInit,ngOnChanges方法。
+ * @example in template: "<div [attr.style]='matrix'>"
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Application } from '../settings/Application';
 
 @Component({
   template: ''
 })
-export class ResizeAble implements OnInit {
+export class ResizeAble implements OnInit, OnDestroy {
 
+  /**
+   * @type {string}
+   * @memberof ResizeAble
+   * @description: this property is for settiing to [attr.style] and adaptive.
+   * @ 此属性用来设置组件模板中的[attr.style],从而或者自适应。
+   */
   public matrix: string = '';
 
+  /**
+   * @type {number}
+   * @memberof ResizeAble
+   * @description: app current height after adaptive.
+   * @ 当前应用自适应后高度
+   */
   public hSet: number = 1625;
   constructor() { }
 
   ngOnInit(){
     window.addEventListener( "resize", this.onResize.bind(this) );
     this.onResize( null );
+  }
+
+  ngOnDestroy(){
+    window.removeEventListener( "resize", this.onResize.bind(this) );
   }
 
   onResize( event: Event | null ){
--- src/basicUI/ui/UIFromParent.ts
+++ src/basicUI/ui/UIFromParent.ts
@@ -1,19 +1,27 @@
-/*
- * @Description: 
- * @version: 1.0
- * @Author: Wayne Yu
- * @Date: 2021-08-31 11:40:50
- * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-09-06 16:50:04
- */
 import { Component, Input, OnInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { TextureData } from '../img/texture-data';
 
+/**
+ * @class UIFromParent
+ * @implements {OnInit}
+ * @implements {OnChanges}
+ * @implements {OnDestroy}
+ * @description: Abstract component, must be extends.The parent of any instance of this class, have to be an instance of "UIComponent". and it has to be an input property "textureData" that comes from parent.
+ * @抽象组件，必须继承。本类组件实例的父级容器，必须是一个"UIComponent"的组件实例;并且,本类有一个属性"textureData"，必须从父级容器传入。
+ * @ In most cases, don't override "ngOnInit" and "ngOnChanges", just initailize the UI items in method "initUI".
+ * @ 不要覆盖ngOnInit,ngOnChanges方法，可以在"initUI"中初始化组件UI
+ */
 @Component({
   template: ''
 })
 export class UIFromParent implements OnInit, OnChanges, OnDestroy{
 
+  /**
+   * @type {TextureData}
+   * @memberof UIFromParent
+   * @description: Material texture object
+   * @ 素材纹理对象
+   */
   @Input()textureData!: TextureData;
   private inited: boolean = false;
 
@@ -23,7 +31,7 @@ export class UIFromParent implements OnInit, OnChanges, OnDestroy{
     if( this.textureData && !this.inited )this.subUIinit();
   }
 
-  subUIinit(){
+  protected subUIinit(){
     if( !this.textureData ) return;
 
     this.initUI();
@@ -35,7 +43,13 @@ export class UIFromParent implements OnInit, OnChanges, OnDestroy{
     if( this.textureData && !this.inited ) this.subUIinit();
   }
 
-  initUI(){
+  /**
+   * @protected
+   * @memberof UIFromParent
+   * @description: Initialize the component UI, and the subclass needs to override
+   * @ 初始化组件UI,子类需覆盖
+   */
+  protected initUI(){
   }
 
   ngOnDestroy(){
