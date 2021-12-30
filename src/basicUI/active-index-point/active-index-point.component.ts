06d25445f53af8f317103f81cf4baf025d49b1db
yuge3005 <yuge3005@163.com>
2021-12-30 12:08:37 +0800

    combine

--- src/basicUI/active-index-point/active-index-point.component.ts
+++ src/basicUI/active-index-point/active-index-point.component.ts
@@ -1,3 +1,4 @@
+import { StyleX } from '../tools/StyleX';
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Rectangle } from '../geom/rectangle';
 /*
@@ -6,7 +7,7 @@ import { Rectangle } from '../geom/rectangle';
 * @Author: Wayne Yu
 * @Date: 2021-09-28 11:08:45
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-11-11 17:25:25
+ * @LastEditTime: 2021-12-30 12:04:34
 */
 
 @Component({
@@ -19,8 +20,8 @@ export class ActiveIndexPointComponent implements OnInit, OnChanges {
   @Input() rect!: Rectangle;
   @Input() items!: any[];
   @Input() activeIndex: number = 0;
-  styleString: string = "";
-  spanStyle: string = "";
+  containnerRect: Object = {};
+  spanStyle: Object = {};
 
   constructor() { }
 
@@ -31,21 +32,11 @@ export class ActiveIndexPointComponent implements OnInit, OnChanges {
     if( changes.rect && this.rect ){
       let diameter: number = this.rect.height;
       let sick: number = Math.round( diameter * 0.3 );
-      this.styleString = `
-        left: ${this.rect.x}px;
-        top: ${this.rect.y}px;
-        width: ${this.rect.width}px;
-        height: ${diameter}px;
-      `;
-      this.spanStyle = `
-        height: ${diameter}px;
-        width: ${diameter}px;
-        -moz-border-radius: ${diameter}px;
-        -webkit-border-radius: ${diameter}px;
-        border-radius: ${diameter}px;
-        border: ${sick}px solid #fff;
-        margin: 0 ${sick}px;
-      `;
+      this.containnerRect = StyleX.setItemToRectangle( this.rect );
+      let borderRadius: Object = StyleX.borderRadius( diameter, false );
+      let spanSize: Object = StyleX.setSize( diameter, diameter );
+      let spanSick: Object = { 'border': sick + 'px solid #fff', 'margin': '0 ' + sick + 'px' };
+      this.spanStyle = StyleX.combine( borderRadius, spanSize, spanSick );
     }
   }
 
--- src/basicUI/tools/StyleX.ts
+++ src/basicUI/tools/StyleX.ts
@@ -1,3 +1,5 @@
+import { Rectangle } from '../geom/rectangle';
+import { Point } from '../geom/point';
 /**
  * @version: 1.0
  * @Author: Wayne Yu
@@ -29,10 +31,78 @@ export class StyleX {
      * @param {number} y
      * @return {*} 
      * @memberof StyleX
-     * @description set the htmlelement position to (x,y)
+     * @description: set the htmlelement position to (x,y)
      * @ 设置网页对象的坐标到(x,y)
      */
-    public static setItemPosition( x: number, y: number ){
+    public static setItemPosition( x: number, y: number ): Object{
         return { 'left': x + 'px', 'top': y + 'px' };
+    }
+
+    /**
+     * @static
+     * @param {Point} pt
+     * @memberof StyleX
+     * @description: set the htmlelement position to the position of a Point
+     * @ 设置网页对象的坐标到Point对象所对应的坐标
+     */
+    public static setItemToPoint( pt: Point ): Object{
+        return this.setItemPosition( pt.x, pt.y );
+    }
+
+    /**
+     * @static
+     * @param {number} x
+     * @param {number} y
+     * @param {number} width
+     * @param {number} height
+     * @return {*} 
+     * @memberof StyleX
+     * @description: set the htmlelement position and size
+     * @ 设置网页元素的位置和大小
+     */
+    public static setItemRect( x: number, y: number, width: number, height: number ): Object{
+        return { 'left': x + 'px', 'top': y + 'px', 'width': width + 'px', 'height': height + 'px' };
+    }
+
+    /**
+     * @static
+     * @param {Rectangle} rect
+     * @memberof StyleX
+     * @description: Set the position and size of the htmlelement according to a Rectangle
+     * @ 根据一个Rectangle对象，来设置网页元素的位置和大小
+     */
+    public static setItemToRectangle( rect: Rectangle ): Object{
+        return this.setItemRect( rect.x, rect.y, rect.width, rect.height );
+    }
+
+    /**
+     * @static
+     * @param {number} width
+     * @param {number} height
+     * @return {*} 
+     * @memberof StyleX
+     * @description: Set the size of the htmlelement
+     * @ 设置网页元素的和大小
+     */
+    public static setSize( width: number, height: number ){
+        return { 'width': width + 'px', 'height': height + 'px' };
+    }
+
+    /**
+     * @static
+     * @param {...Object[]} objs
+     * @return {*}  {Object}
+     * @memberof StyleX
+     * @description: Combine the style Objects to one.
+     * @ 将多个样式对象合并
+     */
+    public static combine( ...objs: any[] ): Object{
+        let newObj: any = {};
+        for( let i: number = 0; i < objs.length; i++ ){
+            for( let ob in objs[i] ){
+                newObj[ob] = objs[i][ob];
+            }
+        }
+        return newObj;
     }
 }
