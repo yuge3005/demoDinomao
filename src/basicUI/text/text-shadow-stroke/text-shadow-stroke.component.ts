e0430b42083af8a59a537702a898fe7b095cacf3
yuge3005 <yuge3005@163.com>
2022-01-07 16:15:55 +0800

    add text-shadow-stroke

---
+++ src/basicUI/text/text-shadow-stroke/text-shadow-stroke.component.ts
@@ -0,0 +1,33 @@
+/*
+* @Description: 
+* @version: 1.0
+* @Author: Wayne Yu
+* @Date: 2022-01-07 15:47:56
+ * @LastEditors: Wayne Yu
+ * @LastEditTime: 2022-01-07 16:13:58
+*/
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { TextFieldComponent } from '../text-field/text-field.component';
+
+@Component({
+  selector: 'app-text-shadow-stroke',
+  templateUrl: './text-shadow-stroke.component.html',
+  styleUrls: ['./text-shadow-stroke.component.css']
+})
+export class TextShadowStrokeComponent extends TextFieldComponent {
+
+  shadowStyle!: string;
+  @Input() shadowObj!: any;
+  @Input() strokeStyle!: Object; 
+
+  constructor() {
+    super();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    super.ngOnChanges( changes );
+    if( this.shadowObj ){
+      this.shadowStyle = this.shadowObj['text-shadow'];
+    }
+  }
+}
