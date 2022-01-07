dd90e9e41c4c738cd8a51ca774274e52f2cedbef
yuge3005 <yuge3005@163.com>
2022-01-07 11:33:09 +0800

    list item

--- src/basicUI/scrollList/list-item.ts
+++ src/basicUI/scrollList/list-item.ts
@@ -4,9 +4,9 @@
 * @Author: Wayne Yu
 * @Date: 2021-09-06 17:03:02
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2022-01-07 11:20:02
+ * @LastEditTime: 2022-01-07 11:29:30
 */
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { UIFromParent } from '../ui/UIFromParent';
 
 @Component({
@@ -16,6 +16,8 @@ export class ListItem extends UIFromParent {
 
   @Input() itemData: any;
   @Input() index: number = 0;
+
+  @Output() itemClick: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {
     super()
