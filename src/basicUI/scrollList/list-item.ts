3e954e90b8057b3d2a2238332bda89246ec1a104
yuge3005 <yuge3005@163.com>
2022-01-07 11:23:25 +0800

    list item

--- src/basicUI/scrollList/list-item.ts
+++ src/basicUI/scrollList/list-item.ts
@@ -4,11 +4,10 @@
 * @Author: Wayne Yu
 * @Date: 2021-09-06 17:03:02
  * @LastEditors: Wayne Yu
- * @LastEditTime: 2021-11-08 09:50:37
+ * @LastEditTime: 2022-01-07 11:20:02
 */
-import { Component, Output, ViewChild, EventEmitter, ElementRef, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { UIFromParent } from '../ui/UIFromParent';
-import { Application } from '../settings/Application';
 
 @Component({
   template: ''
@@ -18,39 +17,7 @@ export class ListItem extends UIFromParent {
   @Input() itemData: any;
   @Input() index: number = 0;
 
-  @Output() itemClick: EventEmitter<any> = new EventEmitter<any>();
-  @ViewChild('itemInList', {static: true}) itemInList!: ElementRef;
-
   constructor() {
     super()
-  }
-
-  subUIinit(){
-    super.subUIinit();
-
-    if( this.itemInList ){
-      if( Application.system.isMobile() ){
-        this.itemInList.nativeElement.addEventListener( "touchend", this.onItemClick.bind(this), true );
-      }
-      else{
-        this.itemInList.nativeElement.addEventListener( "mouseup", this.onItemClick.bind(this), true );
-      }
-    }
-  }
-
-  onItemClick( event: Event ){
-    event.preventDefault();
-    this.itemClick.emit( this.itemData );
-  }
-  
-  ngOnDestroy(): void {
-    if( this.itemInList ){
-      if( Application.system.isMobile() ){
-        this.itemInList.nativeElement.removeEventListener( "touchend", this.onItemClick.bind(this), true );
-      }
-      else{
-        this.itemInList.nativeElement.removeEventListener( "mouseup", this.onItemClick.bind(this), true );
-      }
-    }
   }
 }
